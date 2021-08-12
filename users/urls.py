5c1ebc0a6cb80d2dcd8529a6c9689dbd6d5d977c
Ali Reza Salimi <sa.alireza1999@gmail.com>
2021-08-12 16:47:16 +0430

    tokens paths changed

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('sign-up', UserSignUpView.as_view(), name='sign-up'),
     path('confirm-signup', UserConfirmSingupView.as_view(), name='confirm-signup'),
     path('complete-profile', UserCompleteProfileView.as_view(), name='complete-profile'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
